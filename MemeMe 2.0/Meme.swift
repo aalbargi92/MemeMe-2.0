ac0413d0f82c5612b9a6b9e907061e088219bcee
abdullah <a.albargi92@gmail.com>
2019-10-11 19:13:53 +0300

    Adding Meme Struct

---
+++ MemeMe 2.0/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe 1.0
+//
+//  Created by Abdullah AlBargi on 10/7/19.
+//  Copyright © 2019 Abdullah AlBargi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memeImage: UIImage
+}
